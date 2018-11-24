b402c1a0b01353a1da1156217b9e94aeca02bbb1
mguevara717 <marissa.guevara717@gmail.com>
2018-11-23 23:59:06 -0500

    refactored

--- app/models/apicommunicator.rb
+++ app/models/apicommunicator.rb
@@ -1,19 +1,20 @@
-require 'net/http'
-require 'open-uri'
+require 'rest-client'
 require 'json'
+require 'pry'
 
 class ApiCommunicator
 
-URL = "https://api.betterdoctor.com/2016-03-01/doctors?query=depression&skip=0&limit=30&user_key=21802ea0d6d2c565488582088e578a6c"
+  def get_response_hash(url)
+    response_string = RestClient.get(url)
+    response_hash = JSON.parse(response_string)
+  end
 
- def get_doctors
-   uri = URI.parse(URL)
-   response = Net::HTTP.get_response(uri)
-   response.body
- end
-
+  def get_school_data
+    doctor_data_url = "https://api.betterdoctor.com/2016-03-01/doctors?query=depression&skip=0&limit=30&user_key=21802ea0d6d2c565488582088e578a6c"
+    data_hash = get_response_hash(doctor_data_url)
+  end
 end
 
-doctors = ApiCommunicator.new.get_doctors
+doctors = ApiCommunicator.new.get_school_data
 
 puts doctors
