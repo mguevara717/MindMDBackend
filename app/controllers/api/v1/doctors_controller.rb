df87022db364b7c3da43c8f307bc4bd9dddcd27e
mguevara717 <marissa.guevara717@gmail.com>
2018-11-24 15:11:24 -0500

    controller update

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -5,6 +5,15 @@ class Api::V1::DoctorsController < ApplicationController
     render json: @doctors, status: :accepted
   end
 
+  def create
+    @doctor = Post.create(doctor_params)
+    if @doctor.save
+      render json: @doctor, status: :created
+    else
+      render json: {errors: @doctor.errors.full_messages}, status: :error
+    end
+  end
+
   def show
     @doctor = Doctor.find(params[:id])
     render json: @doctor, status: :found_doctor
