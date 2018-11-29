a3e52d3a0fe41ebcb80c81caea29ae9a4e66c0d9
mguevara717 <marissa.guevara717@gmail.com>
2018-11-29 11:26:16 -0500

    added auth set up for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  has_secured_password
-  validates :username, uniqueness: {case_sensitive: false}
+  has_secure_password
+
+  validates :name, uniqueness: {case_sensitive: false}
 
   has_many :posts
   has_many :reviews
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts, :users, :reviews, :doctors
+
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
     end
   end
 end
