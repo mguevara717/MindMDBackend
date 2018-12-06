81ba31e4ea17bbd56b66af1020863691fcee2b39
mguevara717 <marissa.guevara717@gmail.com>
2018-12-06 02:48:54 -0500

    minor update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
+User.destroy_allx
 Post.destroy_all
 Doctor.destroy_all
 Insurance.destroy_all
